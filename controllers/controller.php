9f38b0f85de542dd20428e92e4e8edecbf568ec7
Dana Clemmer <dclemmer2@mail.greenriver.edu>
2021-02-22 20:06:14 -0500

    added stickiness to form fields

--- controllers/controller.php
+++ controllers/controller.php
@@ -33,6 +33,17 @@ class Controller
     {
         global $dataLayer;
 
+        //Get the data from the POST array
+        $userQuestion = $_POST['question'];
+        $questionOther = $_POST['questionOther'];
+        $userLocation = $_POST['location'];
+        $locationOther = $_POST['locationOther'];
+        $position = $_POST['position'];
+        $userMethod = $_POST['method'];
+        $userIncidentReport = $_POST['incidentReport'];
+        $userIncReportNum = $_POST['incidentNum'];
+        $comments = $_POST['comments'];
+
         //get arrays
         $this->_f3->set('questions', $dataLayer->getQuestions());
         $this->_f3->set('locations', $dataLayer->getLocations());
@@ -40,7 +51,16 @@ class Controller
         $this->_f3->set('methods', $dataLayer->getContactMethods());
         $this->_f3->set('incidentReports', $dataLayer->getIncidentReportOptions());
 
-
+        //make form sticky
+        $this->_f3->set('userQuestion', isset($userQuestion) ? $userQuestion : "");
+        $this->_f3->set('userQuestionOther', isset($questionOther) ? $questionOther : "");
+        $this->_f3->set('userLocation', isset($userLocation) ? $userLocation : "");
+        $this->_f3->set('userLocationOther', isset($locationOther) ? $locationOther : "");
+        $this->_f3->set('userPosition', isset($position) ? $position : "");
+        $this->_f3->set('userMethod', isset($userMethod) ? $userMethod : "");
+        $this->_f3->set('userIncidentReport', isset($userIncidentReport) ? $userIncidentReport : "");
+        $this->_f3->set('userIncReportNum', isset($userIncReportNum) ? $userIncReportNum : "");
+        $this->_f3->set('userComments', isset($comments) ? $comments : "");
 
         //Display a view
         $view = new Template();
